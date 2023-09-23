e8104b1eed0276258a9f752748b46553446098e2
Senichkaa <super-kapanaiko406@ukr.net>
2023-09-23 15:28:01 +0300

    added contactSlice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const contactsStartState = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]
+
+const contactsSlice = createSlice({
+    name:'contacts',
+    initialState: contactsStartState,
+    reducers: {
+        addContact: (state, action) =>  {
+            state.contactsStartState = [action.payload, ...state.contactsStartState]
+        },
+
+        deleteContact: (state,action) => {
+            state.contactsStartState = state.contactsStartState.filter(contact => contact.id !== action.payload)
+        }
+    }
+})
+
+export const contactReducer = contactsSlice.reducer
+export const {addContact, deleteContact} = contactsSlice.actions
--- src/redux/contactsSlice.jsx
+++
@@ -1,7 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const contactsSlice = createSlice({
-    name:'contacts',
-    startState: {contacts: []},
-    reducers: {}
-})
